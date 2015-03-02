92696eada5e7a9437542c2893f6c92759f589584
onur00 <ontur00@gmail.com>
2015-03-02 21:59:22 +0100

    Zakonczone prace nad ArrayBlockingQueue

--- TestBlockingQueue.java
+++ TestBlockingQueue.java
@@ -8,6 +8,8 @@ package Wielowatkowosc;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,14 +21,63 @@ public class TestBlockingQueue {
     
     
     public static void main(String[] args){
-              
+         Thread t1 = new Thread(new Runnable() {
+
+             @Override
+             public void run() {
+                 try {
+                     producer();
+                 } catch (InterruptedException ex) {
+                     Logger.getLogger(TestBlockingQueue.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+             }
+         });
+         
+         Thread t2 = new Thread(new Runnable() {
+
+             @Override
+             public void run() {
+                 try {
+                     consumer();
+                 } catch (InterruptedException e) {
+                     System.err.println("Interrupt exception");
+                     e.printStackTrace();
+                 }
+             }
+         });
+         
+         t1.start();
+         t2.start();
+         
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TestBlockingQueue.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
     }
     
-    public void producer() throws InterruptedException{
+    public static void producer() throws InterruptedException{
      
         Random random = new Random();
         while(true){
-            queue.put(random.nextInt(100));
+            Thread.sleep(random.nextInt(1000));
+            int putValue = random.nextInt(100);
+            queue.put(putValue);
+            System.err.println("Put value " + putValue);
+        }
+    }
+    
+    public static void consumer() throws InterruptedException{
+        Random random = new Random();
+        
+        while(true){
+            Thread.sleep(random.nextInt(1000));
+//            if( random.nextInt(10) == 0){
+                Integer value = queue.take();
+                System.out.println("Size queue " + queue.size() + "  Taken value " + value);
+//            }
         }
     }
 }
