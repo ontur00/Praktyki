dfea9da207316ea98132a663bf15305dccddb5d9
onur00 <ontur00@gmail.com>
2015-03-03 19:24:20 +0100

    Komit przed zmiana galezi   dla pliku MyRunChangeVariable

--- MyRunChangeVariable.java
+++ MyRunChangeVariable.java
@@ -10,96 +10,65 @@ import java.util.logging.Logger;
  *
  * @author Radosław
  */
-public class MyRunChangeVariable {
+public class MyRunChangeVariable implements Runnable {
 
     private int[] tabInt;
     private Random random;
     private static int id;
-    private Object lock = new Object();
-    private long startTime;
-    private long endTime;
 
     public MyRunChangeVariable(int[] tabWsk) {
         id++;
         tabInt = tabWsk;
-        tabInt[0] = 1;
+        tabInt[0] = 0;
 
         if (random == null) {
             random = new Random(System.nanoTime());
         }
     }
 
-//    @Override
-//    public void run() {
-//        synchronized (lock) {
-//            while (true) {
-//                long start = System.nanoTime();
-//                increment(start);
-//                decrement();
-//            }
-//        }
-//    }
-    public void increment(long startTime) throws InterruptedException {
-        //increment
-//        if (tabInt[0] == 0) {
-//            tabInt[0] += 1;
-//            //Sleep thread to Random time        
-//            try {
-//
-//                this.wait(random.nextInt(10000));
-//
-//            } catch (InterruptedException ex) {
-//                Logger.getLogger(MyRunChangeVariable.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//            this.notify();
-//        }
+    @Override
+    public void run() {
 
         while (true) {
-            synchronized (lock) {
-                while (tabInt[0] == 0) {
-                    tabInt[0] = 1;
-//                    System.err.println("Watek " + id + " Increment ");                    
-                    startTime = System.nanoTime();
-                    lock.wait(random.nextInt(1000));
-//                    lock.wait();
-//                    Thread.sleep(random.nextInt(1000));
-                }
-
-                System.err.println("Increment "  + tabInt[0]);
-                lock.notify();
+            synchronized (tabInt) {
+                long start = System.nanoTime();
+                increment(start);
+                decrement();
             }
         }
-
-        //long end = System.nanoTime();
-        //printIncrement(end - startTime);
     }
 
-    public void decrement() throws InterruptedException {
+    public synchronized void increment(long startTime) {
+        //increment
+        if (tabInt[0] == 0) {
+            tabInt[0] += 1;
+            //Sleep thread to Random time        
+            try {
+
+                this.wait(random.nextInt(100));
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MyRunChangeVariable.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            this.notify();
+        }
+        long end = System.nanoTime();
+        printIncrement(end - startTime);
+    }
+
+    private void decrement() {
         long start = 0;
 
-//        if (tabInt[0] == 1) {
-//            tabInt[0] = 0;
-//
-//        }
-//
-//        long end = System.nanoTime();
-//        if ((end - start) < end) {
-//            printDecrement(end - start);
-//        } else {
-//            printDecrement(end);
-//        }
-        while (true) {
-            synchronized (lock) {
-                while (tabInt[0] == 1) {
-                    tabInt[0] = 0;
-                    endTime = System.nanoTime();
+        if (tabInt[0] == 1) {
+            tabInt[0] = 0;
 
-                    lock.wait();
-                }
+        }
 
-                System.out.println("decrement " + tabInt[0]);
-                lock.notify();
-            }
+        long end = System.nanoTime();
+        if ((end - start) < end) {
+            printDecrement(end - start);
+        } else {
+            printDecrement(end);
         }
     }
 
